705cc5515db53ea82e6093426b9d61a67d04ba0d
Haris Mohammad <hmohammad@spartaglobal.com>
2015-11-27 11:41:54 +0000

    finished all the core tests

--- spec/httparty_tests_spec.rb
+++ spec/httparty_tests_spec.rb
@@ -11,6 +11,7 @@ describe "Test Suite sends a post request" do
      #verify
       expect(id['title']).to eq("An Item")
 	    expect(id.code).to eq(201)
+      expect(id.message).to eq("Created")
       expect(id['due']).to eq("2015-01-01")
     #teardown
       TodosUtil.delete(id['id'])
@@ -23,9 +24,15 @@ describe "Test Suite sends a post request" do
   end
   it "should fail to make a post without any arguments" do
       r= HTTParty.post "http://lacedeamon.spartaglobal.com/todos", query:{} 
-     #verify
+      #verify
       expect(r.code).to eq(422)
       expect(r.message).to eq("Unprocessable Entity")
+  end
+   it "should fail to make a post with specific id" do
+      r= HTTParty.post "http://lacedeamon.spartaglobal.com/todos/2248", query:{title: "An Item", due: "01-01-2012"} 
+      #verify
+      expect(r.code).to eq(405)
+      expect(r.message).to eq("Method Not Allowed")
   end
 end
 
@@ -35,6 +42,7 @@ describe "Test Suite sends a get request" do
       r= HTTParty.get "http://lacedeamon.spartaglobal.com/todos/#{id['id']}"
       expect(r["title"]).to eq("An Item")
       expect(r.code).to eq(200)
+      expect(r.message).to eq("OK")
       expect(r['due']).to eq("2015-01-01")
       #teardown
       TodosUtil.delete(id['id'])
@@ -59,6 +67,7 @@ describe "Test Suite sends a put request" do
     expect(r["title"]).to eq("Changed An Item to Another Item")
    
     expect(r.code).to eq(200)
+    expect(r.message).to eq("OK")
     expect(r['due']).to eq("2015-01-01")
     #teardown
     TodosUtil.delete(r['id'])
@@ -66,11 +75,12 @@ describe "Test Suite sends a put request" do
 end
 
 describe "Test Suite sends a patch request" do
-  it "should" do
+  it "should check whether that specific post was modified" do
     id = TodosUtil.post
     r= HTTParty.patch "http://lacedeamon.spartaglobal.com/todos/#{id['id']}", query:{title: "Changed An Item only but not due date"}
     expect(r["title"]).to eq("Changed An Item only but not due date")
     expect(r.code).to eq(200)
+    expect(r.message).to eq("OK")
     expect(r['due']).to eq("2015-01-01")
     #teardown
     TodosUtil.delete(r['id'])
@@ -78,21 +88,19 @@ describe "Test Suite sends a patch request" do
 end
 
 describe "Test suite sends a delete request" do
-  it "should delete a single todo" do
-   
+  it "should delete a single todo" do  
     id = TodosUtil.post
     delete=  TodosUtil.delete id['id']
     expect(delete['id']).to eq (nil)  
     expect(delete.code).to eq(204)
+    expect(delete.message).to eq("No Content")
     expect(delete.content_type).to eq(nil)
-
-  end
-  
+  end  
   it "should not delete todo collection" do
-    
-  end
-  
-  it "should not delete todo collection" do
-
+    delete = HTTParty.delete "http://lacedeamon.spartaglobal.com/todos/"
+    expect(delete['id']).to eq (nil)  
+    expect(delete.code).to eq(405)
+    expect(delete.message).to eq("Method Not Allowed")
+    expect(delete.content_type).to eq("text/html")
   end
 end
