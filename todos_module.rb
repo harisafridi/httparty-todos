dad80ccb1b30f473cb9f495fc99a7f839baa1cf7
Haris <hmohammad@spartaglobal.com>
2015-11-27 17:08:54 +0000

    Update todos_module.rb

--- todos_module.rb
+++ todos_module.rb
@@ -1,15 +1,28 @@
 require 'httparty'
 require 'yaml'
 
+
 module TodosUtil
- def self.post
-	HTTParty.post "http://lacedeamon.spartaglobal.com/todos", 
-	query:{title: "An Item", due: "2015-01-01"}
- end
- def self.delete(id)
- 	 HTTParty.delete "http://lacedeamon.spartaglobal.com/todos/#{id}"
- end
- def self.get(id)
- 	 HTTParty.delete "http://lacedeamon.spartaglobal.com/todos/#{id}"
- end
+
+   @data = YAML.load(File.open('links.yml'))
+
+   def self.post
+
+      post = @data['urls'][0]["post"]
+      title = @data['urls'][0]["title"]
+    	HTTParty.post post, query:{title: "#{title}", due: "2015-01-01"}
+
+   end
+   def self.get(id)
+
+     	get = @data['urls'][0]["get"]
+     	HTTParty.get "#{get}#{id}"
+
+   end
+   def self.delete(id)
+
+     	delete = @data['urls'][0]["delete"]
+     	HTTParty.delete "#{delete}#{id}"
+
+   end
 end
